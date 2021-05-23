55b51f6eb89f600c68f5844f5bf70ea41bf66b8f
Arturo <47676848+ArturoSbr@users.noreply.github.com>
2021-05-22 20:30:01 -0500

    Added docstring and grid parameters

--- decision_tree_imputer.py
+++ decision_tree_imputer.py
@@ -1,58 +1,163 @@
-import numpy as np
-from sklearn.tree import DecisionTreeRegressor
+# Import pyplot
+import matplotlib.pyplot as plt
 
-def impute(data, to_impute, max_depth=5, max_features=5, random_state=None):
+# Plot a line chart on each of the two y-axes
+def line_line(x, y1, y2, y1_color='C0', y2_color='C1',
+              title=None, x_label=None, y1_label=None, y2_label=None,
+              y1_legend=None, y2_legend=None, y1_lim=None, y2_lim=None,
+              y1_grid=False, y2_grid=False, export_as='fig.png'):
     '''
-    Multivariate feature imputation method that imputes the missing values of a feature using Classification and
-    Regression Trees. One independent variable is modeled as a function of other independent variables and
-    its missing values are filled in accordance to the resulting model.
+    Plot two series on a dual y-axis chart. Both series are plotted as line
+    charts and must be of equal length.
+
+    Parameters
+    ----------
+        x: array-like
+            Values to plot along the x-axis.
+        y1: array-like
+            Values to plot along the y-axis on the left side.
+        y2: array-like
+            Values to plot along the y-axis on the right side.
+        y1_color: str
+            Color of `y1`.
+        y2_color: str
+            Color of `y2`.
+        title: str
+            Title of the chart.
+        x_label: str
+            Label for the x-axis.
+        y1_label: str
+            y-axis label for `y1`.
+        y2_label: str
+            y-axis label for `y2`.
+        y1_legend: str
+            Legend label for `y1`.
+        y2_legend: str
+            Legend label for `y2`.
+        y1_lim: tuple
+            Tuple of length 2. `y1_lim[0]` and `y1_lim[1]` are set as the
+            minimum and maximum values for `y1` on the y-axis.
+        y2_lim: tuple
+            Tuple of length 2. `y2_lim[0]` and `y2_lim[1]` are set as the
+            minimum and maximum values for `y2` on the y-axis.
+        y1_grid: bool, default False
+            If True, the chart will display horizontal lines on the major ticks
+            of `y1`.
+        y2_grid: bool, default False
+            If True, the chart will display horizontal lines on the major ticks
+            of `y2`.
+        export_as: str
+            Path and filename to export the chart.
     
-        Parameters
-        ----------
-                data : pandas.DataFrame object
-                    The dataset that contains *all* the independent variables, *including* the feature containing missing values.
-                to_impute : str
-                    The name of the feature to be modeled as a function of the other independent variables. `to_impute` must
-                    be a column in `data`.
-                max_depth : int, default `5`
-                    The maximum depth of the resulting decision tree.
-                max_features : int, default `5`
-                    The maximum number of features used to model `to_impute`.
-        Returns
-        -------
-                numpy.array
-                    The output is a numpy array that matches the length of the original dataset.
-                    Entries that were not empty retain their original value.
-                    Entries that used to be `NaN` are filled in by the predicted value of the regressor/classifier.
-        Example
-        -------
-                df['feature_1_imputed'] = decision_tree_imputer(data=df, to_impute='feature_1')
-        GitHub
-        ------
-                Visit https://github.com/ArturoSbr/ for more content
+    Returns
+    -------
+        Figure generated with `matplotlib.pyplot.subplots(nrows=1, ncols=1)`.
+    
+    GitHub
+    ------
+        https://github.com/ArturoSbr/
     '''
-    features = data.columns.tolist()
-    features.remove(to_impute)
-    X = data.loc[data[to_impute].notna(), features].copy()
-    y = data.loc[data[to_impute].notna(), to_impute].copy()
-    m = data[features].isna().sum()
-    X = X.drop(m[m > 0].index, axis=1)
-    features = X.columns
-    if len(features) == 0:
-        raise 'All the independent features have missing values. At least one feature must be completely filled in.'
-    else:
-        if len(features) == 1:
-            X = X.values
-            clf = DecisionTreeRegressor(criterion='mse', splitter='best', max_depth=max_depth,
-                                        max_features=max_features, random_state=random_state)
-            clf.fit(X, y)
-            return np.where(data[to_impute].isna(),
-                            clf.predict(data[features].values),
-                            data[to_impute].values)
-        else:
-            clf = DecisionTreeRegressor(criterion='mse', splitter='best', max_depth=max_depth,
-                                        max_features=max_features, random_state=random_state)
-            clf.fit(X, y)
-            return np.where(data[to_impute].isna(),
-                            clf.predict(data[features]),
-                            data[to_impute].values)
+    fig, ax1 = plt.subplots()
+    ax1.plot(y1, color=y1_color, lw=3, label=y1_legend)
+    ax1.set_xlabel(x_label)
+    ax1.set_ylabel(y1_label)
+    ax1.set_xticks(range(len(x)))
+    ax1.set_xticklabels(labels=x, rotation=45)
+    if type(y1_lim) is tuple:
+        ax1.set_ylim(bottom=y1_lim[0], top=y1_lim[1])
+    if y1_grid:
+        ax1.grid(axis='y')
+    ax2 = ax1.twinx()
+    ax2.plot(y2, color=y2_color, lw=3, label=y2_legend)
+    ax2.set_ylabel(y2_label, rotation=270, labelpad=13)
+    if type(y2_lim) is tuple:
+        ax2.set_ylim(bottom=y2_lim[0], top=y2_lim[1])
+    if y2_grid:
+        ax2.grid(axis='y')
+    if (type(y1_legend) is str) or (type(y2_legend) is str):
+        fig.legend(bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
+    plt.title(title)
+    if type(export_as) == str:
+        plt.savefig(export_as, dpi=300, bbox_inches='tight')
+    plt.show()
+
+# Plot a bar chart on the primary y-axis and a line chart on the secondary y-axis
+def bar_line(x, y1, y2, y1_color='C0', y2_color='C1',
+             title=None, x_label=None, y1_label=None, y2_label=None,
+             y1_legend=None, y2_legend=None, y1_lim=None, y2_lim=None,
+             y1_grid=False, y2_grid=False, export_as=None):
+    '''
+    Plot two series on a dual y-axis chart. The first series is plotted as a
+    bar chart and the second one as a line chart. Both series must be of equal
+    length.
+
+    Parameters
+    ----------
+        x: array-like
+            Values to plot along the x-axis.
+        y1: array-like
+            Values to plot along the y-axis on the left side.
+        y2: array-like
+            Values to plot along the y-axis on the right side.
+        y1_color: str
+            Color of `y1`.
+        y2_color: str
+            Color of `y2`.
+        title: str
+            Title of the chart.
+        x_label: str
+            Label for the x-axis.
+        y1_label: str
+            y-axis label for `y1`.
+        y2_label: str
+            y-axis label for `y2`.
+        y1_legend: str
+            Legend label for `y1`.
+        y2_legend: str
+            Legend label for `y2`.
+        y1_lim: tuple
+            Tuple of length 2. `y1_lim[0]` and `y1_lim[1]` are set as the
+            minimum and maximum values for `y1` on the y-axis.
+        y2_lim: tuple
+            Tuple of length 2. `y2_lim[0]` and `y2_lim[1]` are set as the
+            minimum and maximum values for `y2` on the y-axis.
+        y1_grid: bool, default False
+            If True, the chart will display horizontal lines on the major ticks
+            of `y1`.
+        y2_grid: bool, default False
+            If True, the chart will display horizontal lines on the major ticks
+            of `y2`.
+        export_as: str
+            Path and filename to export the chart.
+    
+    Returns
+    -------
+        Figure generated with `matplotlib.pyplot.subplots(nrows=1, ncols=1)`.
+    
+    GitHub
+    ------
+        https://github.com/ArturoSbr/
+    '''
+    fig, ax1 = plt.subplots()
+    ax1.bar(range(len(x)), y1, color=y1_color, label=y1_legend)
+    ax1.set_xlabel(x_label)
+    ax1.set_ylabel(y1_label)
+    ax1.set_xticks(range(len(x)))
+    ax1.set_xticklabels(labels=x, rotation=45)
+    if type(y1_lim) is tuple:
+        ax1.set_ylim(bottom=y1_lim[0], top=y1_lim[1])
+    ax2 = ax1.twinx()
+    ax2.plot(y2, color=y2_color, lw=3, label=y2_legend)
+    ax2.set_ylabel(y2_label, rotation=270, labelpad=14)
+    if type(y2_lim) is tuple:
+        ax2.set_ylim(bottom=y2_lim[0], top=y2_lim[1])
+    if y1_grid:
+        ax1.grid(axis='y')
+    if y2_grid:
+        ax2.grid(axis='y')
+    if (type(y1_legend) is str) or (type(y2_legend) is str):
+        fig.legend(bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
+    plt.title(title)
+    if type(export_as) == str:
+        plt.savefig(export_as, dpi=300, bbox_inches='tight')
+    plt.show()
